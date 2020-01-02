995bea250ee97ddf8e2248fbb24d21aab7e04d69
baoxingyu <baoxingyubob@outlook.com>
2020-01-02 23:14:29 +0800

    更新版本，1）完善了编译方式compile.py。2）所有的内部变量都以‘_’开头。外部不能访问。

--- compile.py
+++ compile.py
@@ -7,7 +7,34 @@ os.environ['PATH'] = path
 
 dst = dstdir + name + '.exe'
 
-if os.system('pyinstaller -F --clean -w {}.py'.format(name)):
+exestr = 'pyinstaller -F --clean ' + ' -w {}.py'.format(name)
+print(exestr)
+
+# 当发生RecursionError时添加的补丁
+patch = """
+import sys
+sys.setrecursionlimit(100000)
+"""
+patch = [p.strip() + '\n' for p in patch.split('\n')]
+
+# 子程序返回值
+r = os.system(exestr)
+
+if r:
+    print('error happened when using pyinstaller, try set larger maximum recursion depth.')
+    file = '{}.spec'.format(name)
+    tempfile = '_' + file
+    with open(file, 'r') as fp, open(tempfile, 'w') as gp:
+        lines = fp.readlines()
+        lines = lines[0:2] + patch + lines[1:]
+        gp.writelines(lines)
+    os.remove(file)
+    os.rename(tempfile, file)
+    exestr = 'pyinstaller {}'.format(file)
+    print(exestr)
+    r = os.system(exestr)
+
+if r:
     print('error happened when using pyinstaller')
 else:
     if not os.path.isdir(dstdir):
@@ -16,4 +43,3 @@ else:
         os.remove(dst)
     print('save to %s.' % dst)
     shutil.copy('./dist/{}.exe'.format(name), dst)
-    print('done.')
--- scicalculator.py
+++ scicalculator.py
@@ -1,10 +1,10 @@
-import PySimpleGUI as sg
+import PySimpleGUI as _sg
 
 from scipy.constants import *
 from numpy import *
 
-help_text = """
-version: 0.1
+_help_text = """
+version: 0.2
 
 使用了
 ```
@@ -23,77 +23,83 @@ version: 0.1
 _history = []
 _p = -1
 
-def validate(text):
+def _validate(text):
+    for s in text.split():
+        if s.strip().startswith('_'):
+            return False
+        if s in ('eval', 'exec'):
+            return False
     for s in ['=', 'import', 'from', ';']:
         if s in text:
             return False
     return True
 
-menu_def = [['&Help', '&About']]
+
+_menu_def = [['&Help', '&About']]
 # All the stuff inside your window.
-main_layout = [[sg.Menu(menu_def)],
-          [sg.Text('公式:'), sg.InputText(key='-IN-')],
-          [sg.Text('结果:'), sg.Text('', size=(40, 1), key='-OUTPUT-')],
-          [sg.Multiline(tooltip='history', disabled=True, key='HISTORY'), sg.Button('Save')],
-          [sg.Button('Calculate', bind_return_key=True), sg.Button('Exit')]]
+_main_layout = [[_sg.Menu(_menu_def)],
+          [_sg.Text('公式:'), _sg.InputText(key='-IN-')],
+          [_sg.Text('结果:'), _sg.Text('', size=(40, 1), key='-OUTPUT-')],
+          [_sg.Multiline(tooltip='history', disabled=True, key='HISTORY'), _sg.Button('Save')],
+          [_sg.Button('Calculate', bind_return_key=True), _sg.Button('Exit')]]
 
 # Create the Window
-window = sg.Window('小型科学计算器', main_layout, return_keyboard_events=True)
+_window = _sg.Window('小型科学计算器', _main_layout, return_keyboard_events=True)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event, values = window.read()
-    if event == 'Up:38':
+    _event, _values = _window.read()
+    if _event == 'Up:38':
         try:
             if _p-1<0:
                 continue
-            window['-IN-'].Update(_history[_p-1])
+            _window['-IN-'].Update(_history[_p-1])
         except IndexError:
             pass
         else:
             _p -= 1
-    if event == 'Down:40':
+    if _event == 'Down:40':
         try:
-            window['-IN-'].Update(_history[_p+1])
+            _window['-IN-'].Update(_history[_p+1])
         except IndexError:
             pass
         else:
             _p += 1
-    if event in (None, 'Exit', 'Escape:27'):   # if user closes window or clicks cancel
+    if _event in (None, 'Exit', 'Escape:27'):   # if user closes window or clicks cancel
         break
-    if event in ('About', 'F1:112'):
-        sg.Popup(help_text, title='帮助文件')
-    if event == 'Save':
-        save_layout = [[sg.Text('Filename')],
-                       [sg.Input(), sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
-                       [sg.OK(), sg.Cancel()]]
-        save_window = sg.Window('Save History', save_layout)
-        event, values = save_window.read()
-        save_window.close()
-        if event == 'OK':
-            filename = values[0]
+    if _event in ('About', 'F1:112'):
+        _sg.Popup(_help_text, title='帮助文件')
+    if _event == 'Save':
+        _save_layout = [[_sg.Text('Filename')],
+                       [_sg.Input(), _sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
+                       [_sg.OK(), _sg.Cancel()]]
+        _save_window = _sg.Window('Save History', _save_layout)
+        _event, _values = _save_window.read()
+        _save_window.close()
+        if _event == 'OK':
+            _filename = _values[0]
             try:
-                with open(filename, 'w', encoding='utf-8') as fp:
-                    fp.write(window['HISTORY'].Get())
-                sg.popup('save history to', filename)
+                with open(_filename, 'w', encoding='utf-8') as fp:
+                    fp.write(_window['HISTORY'].Get())
+                _sg.popup('save history to', _filename)
             except:
                 continue
-    if event == 'Calculate':
-        inp = values['-IN-']
+    if _event == 'Calculate':
+        _inp = _values['-IN-']
         try:
-            if not validate(inp):
+            if not _validate(_inp):
                 raise SyntaxError('invalid input')
-            res = str(eval(inp))
+            _res = str(eval(_inp))
         except Exception as e:
-            res = str(e)
-        window['-OUTPUT-'].Update(res)
-        window['HISTORY'].Update(inp + '  -->  ' + res + '\n', append=True, autoscroll=True)
-        _history.append(inp)
+            _res = str(e)
+        _window['-OUTPUT-'].Update(_res)
+        _window['HISTORY'].Update(_inp + '  -->  ' + _res + '\n', append=True, autoscroll=True)
+        _history.append(_inp)
         _p = len(_history)-1
-    if event in ('Control_L:17', 'Control_R:17'):
-        event, _ = window.read(timeout=500)
-        if event in ('f', 'F'):
-            window['-IN-'].Update('')
-            window['-OUTPUT-'].Update('')
+    if _event in ('Control_L:17', 'Control_R:17'):
+        _event, _ = _window.read(timeout=500)
+        if _event in ('f', 'F'):
+            _window['-IN-'].Update('')
+            _window['-OUTPUT-'].Update('')
 
-window.close()
+_window.close()
